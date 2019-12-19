95706c94547279884826682245766bb12d3b642d
Kyra Mellema <kemellema@gmail.com>
2019-12-19 13:34:17 -0500

    merge

--- src/components/Cust_DashBoard/hairprofile.jsx
+++ src/components/Cust_DashBoard/hairprofile.jsx
@@ -4,55 +4,50 @@ import firedatabase from "../auth/base";
 import Moment from "react-moment";
 import "../../Stylesheets/global.css";
 import "../../Stylesheets/hairprofile.css";
-<<<<<<< HEAD
-import sean from "../../images/sean.JPG";
-=======
-
 import person from "../../images/person.png";
->>>>>>> 7f9ba0975f75e6b20d4fb8d3a8817683ccdeda4e
 
 class HairProfile extends React.Component {
-  state = {
-    myprofile: [],
-    uid: " "
-  };
-  componentDidMount = () => {
-    this.fetchCurrentUser();
-  };
-  fetchCurrentUser = async () => {
-    var user = firedatabase.auth().currentUser;
-    var uid;
-    if (user != null) {
-      //   name = user.displayName;
-      //   email = user.email;
-      //   photoUrl = user.photoURL;
-      //   emailVerified = user.emailVerified;
-      uid = user.uid;
-    }
-    this.setState({
-      uid: user.uid
-    });
-    firebase
-      .firestore()
-      .collection("Quiz_list")
-      .where("userid", "==", user.uid)
-      .get()
-      .then(querySnapshot => {
-        const data = querySnapshot.docs.map(doc => doc.data());
-        this.setState({ myprofile: data });
-      })
-      .catch(function(error) {
-        console.log("Error getting documents: ", error);
-      });
-  };
+	state = {
+		myprofile: [],
+		uid: " "
+	};
+	componentDidMount = () => {
+		this.fetchCurrentUser();
+	};
+	fetchCurrentUser = async () => {
+		var user = firedatabase.auth().currentUser;
+		var uid;
+		if (user != null) {
+			//   name = user.displayName;
+			//   email = user.email;
+			//   photoUrl = user.photoURL;
+			//   emailVerified = user.emailVerified;
+			uid = user.uid;
+		}
+		this.setState({
+			uid: user.uid
+		});
+		firebase
+			.firestore()
+			.collection("Quiz_list")
+			.where("userid", "==", user.uid)
+			.get()
+			.then(querySnapshot => {
+				const data = querySnapshot.docs.map(doc => doc.data());
+				this.setState({ myprofile: data });
+			})
+			.catch(function(error) {
+				console.log("Error getting documents: ", error);
+			});
+	};
 
-  render() {
-    const { myprofile } = this.state;
+	render() {
+		const { myprofile } = this.state;
 
 		return (
 			<section className="dashSections">
 				<div className="profile-box ">
-					<img className="img-fluid" src={sean} alt="Sean" />
+					<img className="img-fluid" src={person} alt="person" />
 					{myprofile.map(myprofiles => (
 						<>
 							<ul className="profile">
